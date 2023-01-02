06bce7ea4fbdee7a044dad24ce0ed5ade874c7f6
alex <o.lototskyi@gmail.com>
2023-01-02 10:20:20 +0200

    using redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()));
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+        .then(data => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()));
+}
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
@@ -12,25 +13,25 @@ const stringMiddleware = (store) => (next) => (action) => {
      return next(action);
 };
 
-const enhancer = (createStore) => (...args) => {
-     const store = createStore(...args);
+// const enhancer = (createStore) => (...args) => {
+//      const store = createStore(...args);
 
-     const oldDispatch = store.dispatch;
-     store.dispatch = (action) => {
-          if (typeof action === 'string') {
-               return oldDispatch({
-                    type: action
-               });
-          }
-          return oldDispatch(action);
-     }
-     return store;
-};
+//      const oldDispatch = store.dispatch;
+//      store.dispatch = (action) => {
+//           if (typeof action === 'string') {
+//                return oldDispatch({
+//                     type: action
+//                });
+//           }
+//           return oldDispatch(action);
+//      }
+//      return store;
+// };
 
 const store = createStore(
      combineReducers({heroes, filters}),
      compose(
-          applyMiddleware(stringMiddleware),
+          applyMiddleware(ReduxThunk, stringMiddleware),
           window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
      ));
 
